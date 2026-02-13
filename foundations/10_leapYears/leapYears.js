59961ebeb827a425ebaa5ab77198ec7c690a2369
furankiesama <franknwokoma1@gmail.com>
2026-02-13 17:14:49 +0100

    add leapYears function
    checks if year is leap year

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const divisbleByFour = year % 4 == 0;
+    const isCenturyYear = (year % 100 == 0) 
+    const isLeapYear = (year % 400 == 0)
+    if(divisbleByFour && !isCenturyYear || (year % 400 == 0) ) {
+        return true;
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
