d48f548b915e495a87ebb64a401c5bbb12d7f270
furankiesama <franknwokoma1@gmail.com>
2026-02-13 14:45:27 +0100

    add removeFromArray function
    Removes other elements specified arguments from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...number) {
+    const multipleElements = new Set(number);
+
+    return originalArray.filter(item => !multipleElements.has(item))
 };
 
 // Do not edit below this line
