d249ae82f0f5d673b4199210ac0f19ff8405f7f2
furankiesama <franknwokoma1@gmail.com>
2026-02-13 12:48:53 +0100

    add function repeatString
    Repeats the string a specified amount of times

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (text, num) {
+  if (num < 0) return "ERROR";
 
+  let finalString = "";
+
+  for (let index = 0; index < num; index++) {
+    finalString += text;
+  }
+  return finalString;
 };
 
 // Do not edit below this line
