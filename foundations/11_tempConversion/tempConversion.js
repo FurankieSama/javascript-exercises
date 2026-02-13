cb80d45d577ced2249b35a3c284c6ad3a6f56f7e
furankiesama <franknwokoma1@gmail.com>
2026-02-13 19:02:53 +0100

    add functions to convert temperature

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,12 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = (valueinFahrenheit) =>  Number(((valueinFahrenheit - 32) * (5/9)).toFixed(1))
 
-const convertToFahrenheit = function() {
-};
+
+const convertToFahrenheit = (valueInCelsius) =>  Number(((valueInCelsius * 1.8) + 32).toFixed(1));
+
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+ 
